93a3a3afd81e546e426360d7d61079812173fe1d
kindyluv <onyeukwuamara@gmail.com>
2021-10-16 19:52:36 +0100

    added classDb

--- src/Models/Teacher.java
+++ src/Models/Teacher.java
@@ -2,4 +2,73 @@ package Models;
 
 public class Teacher extends Staffs{
 
+
+    public Teacher(String firstName, String lastName, String dateOfBirth, Gender gender) {
+        super(firstName, lastName, dateOfBirth, gender);
+    }
+
+    public Teacher(String firstName, String lastName, String dateOfBirth, Gender gender, Address address) {
+        super(firstName, lastName, dateOfBirth, gender, address);
+    }
+
+    @Override
+    public String getFirstName() {
+        return super.getFirstName();
+
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        super.setFirstName(firstName);
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName();
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        super.setLastName(lastName);
+    }
+
+    @Override
+    public String getDateOfBirth() {
+        return super.getDateOfBirth();
+    }
+
+    @Override
+    public void setDateOfBirth(String dateOfBirth) {
+        super.setDateOfBirth(dateOfBirth);
+    }
+
+    @Override
+    public long getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(long id) {
+        super.setId(id);
+    }
+
+    @Override
+    public Gender getGender() {
+        return super.getGender();
+    }
+
+    @Override
+    public void setGender(Gender gender) {
+        super.setGender(gender);
+    }
+
+    @Override
+    public Address getAddress() {
+        return super.getAddress();
+    }
+
+    @Override
+    public void setAddress(Address address) {
+        super.setAddress(address);
+    }
 }
---
+++ src/db/ClassDb.java
@@ -0,0 +1,18 @@
+package db;
+
+import Models.Class;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassDb {
+    private final List<Class> classes = new ArrayList<>();
+
+    public Class findById(long id) {
+        return classes.get((int) id);
+    }
+
+//    public void add(Class newClass) {
+//        classes.add(newClass);
+//    }
+}
