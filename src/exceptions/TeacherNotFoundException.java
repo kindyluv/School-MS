809c1988308f23247f300a877d94c3b940661f46
kindyluv <onyeukwuamara@gmail.com>
2021-10-17 21:36:49 +0100

    added TeacherServiceInterface

---
+++ src/exceptions/TeacherNotFoundException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+public class TeacherNotFoundException extends Throwable{
+
+    TeacherNotFoundException() {
+        super();
+    }
+
+    public TeacherNotFoundException(String message){
+        super(message);
+    }
+
+    TeacherNotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    TeacherNotFoundException(Throwable cause){
+        super(cause);
+    }
+}
