b237bb256bbf3ebd6b24de22616e37d30edc4558
kindyluv <onyeukwuamara@gmail.com>
2021-10-17 21:38:20 +0100

    added TeacherExceptionClass

--- src/exceptions/TeacherNotFoundException.java
+++ src/exceptions/TeacherNotFoundException.java
@@ -1,20 +1,20 @@
 package exceptions;
 
-public class TeacherNotFoundException extends Throwable{
+public class TeacherNotFoundException extends Throwable {
 
     TeacherNotFoundException() {
         super();
     }
 
-    public TeacherNotFoundException(String message){
+    public TeacherNotFoundException(String message) {
         super(message);
     }
 
-    TeacherNotFoundException(String message, Throwable cause){
+    TeacherNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    TeacherNotFoundException(Throwable cause){
+    TeacherNotFoundException(Throwable cause) {
         super(cause);
     }
 }
