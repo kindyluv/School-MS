6c9918a9721da469e333a4ddc02412ebfb1b5317
kindyluv <onyeukwuamara@gmail.com>
2021-10-18 15:15:19 +0100

    added Class class

--- src/Models/Class.java
+++ src/Models/Class.java
@@ -1,8 +1,40 @@
 package Models;
 
 public class Class {
-    private Teacher teacher;
+    private long id;
     private String name;
-    private Student student;
-    private String id;
+    private static int count = 0;
+
+    public Class(){
+        this.id = ++count;
+    }
+
+    public Class(String name) {
+        this.id = ++count;
+        this.name = name;
+    }
+
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getCount() {
+//        return count;
+//    }
+//
+//    public void setCount(int count) {
+//        this.count = count;
+//    }
 }
